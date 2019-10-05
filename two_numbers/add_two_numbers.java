be60bd56e0f742eb104cc2bc6774870c08b76cb2
Kushal Kewlani <kkewlani@amazon.com>
2019-10-05 15:33:39 -0700

    Increasing the calculation limit by using BigDecimal

--- two_numbers/add_two_numbers.java
+++ two_numbers/add_two_numbers.java
@@ -23,19 +23,22 @@
  * }
  */
 
+import java.math.BigDecimal;
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int l1Length, l2Length;
-        int number1, number2;
+        BigDecimal number1, number2;
         number1 = generateNumber(l1);
         number2 = generateNumber(l2);
         //System.out.println(number1 + " "+ number2);
-        int addedNumber = number1 + number2;
+        BigDecimal addedNumber = number1.add(number2);
         return generateList(addedNumber);
     }
 
-    private int generateNumber(ListNode l) {
-        int pos = 0, number = 0;
+    private BigDecimal generateNumber(ListNode l) {
+        int pos = 0;
+        BigDecimal number = new BigDecimal(0);
         while (l != null) {
             number = addDigit(number, l.val, pos);
             //System.out.println(number);
@@ -45,14 +48,16 @@ class Solution {
         return number;
     }
 
-    private int addDigit(int number, int digit, int pos ) {
-        int newNumber = (number + digit * ((int) Math.pow(10,pos)));
-        //System.out.println(newNumber + ", ("+ number+ "+ "+ digit +"* (10 ^"+pos+")");
+    private BigDecimal addDigit(BigDecimal number, int digit, int pos ) {
+        BigDecimal posNumber = (new BigDecimal (10)).pow(pos);
+
+        BigDecimal newNumber = number.add(posNumber.multiply(new BigDecimal(digit)));
+        //System.out.println(newNumber.toPlainString() + ", ("+ number.toPlainString()+ "+ "+ posNumber.toPlainString());
         return newNumber;
     }
 
-    private ListNode generateList(int number) {
-        String num = String.valueOf(number);
+    private ListNode generateList(BigDecimal number) {
+        String num = number.toPlainString();
         int numLength = num.length();
         ListNode prev = null, curr = null, head = null;
         for (int i = numLength-1; i >= 0; i--) {
