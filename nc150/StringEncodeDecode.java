a526e5bf6bf178549b4b29fffc8be535cb22aa6c
Kushal Kewlani <kkewlani@gmail.com>
2024-09-27 07:48:51 -0700

    String Encode Decode

---
+++ nc150/StringEncodeDecode.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+/**
+ * String Encode and Decode
+ * Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+ *
+ * Please implement encode and decode
+ *
+ * Example 1:
+ *
+ * Input: ["neet","code","love","you"]
+ *
+ * Output:["neet","code","love","you"]
+ * Example 2:
+ *
+ * Input: ["we","say",":","yes"]
+ *
+ * Output: ["we","say",":","yes"]
+ * Constraints:
+ *
+ * 0 <= strs.length < 100
+ * 0 <= strs[i].length < 200
+ * strs[i] contains only UTF-8 characters.
+ */
+
+/**
+ * Clarifying ques should be:
+ * 1. are there only words? or can be sentences ?
+ * 2. what special characters can be part fo the string?
+ * 3. behavior when string is null and empty in input for encoding
+ * 4. expected behavior with empty or null list
+ */
+
+// This uses stringBuffer isEmpty method - available from Java15 + 
+public class StringEncodeDecode {
+    public String encode(List<String> strs) {
+        if(strs==null || strs.isEmpty()) {
+            return null;
+        }
+        StringBuffer st = new StringBuffer();
+        for (String str: strs) {
+            st.append(str).append("|");
+        }
+        st.deleteCharAt(st.length()-1);
+        return st.toString();
+    }
+
+    public List<String> decode(String str) {
+        if(str == null) {
+            return new ArrayList<String>();
+        }
+        if(str.length() == 0) {
+            return Collections.singletonList("");
+        }
+        StringBuffer buf = new StringBuffer();
+        List<String> result = new ArrayList<>();
+        for (char c : str.toCharArray()) {
+            if (c=='|') {
+                result.add(buf.toString());
+                buf = new StringBuffer();
+            } else {
+                buf.append(c);
+            }
+        }
+        if(!buf.isEmpty()) {
+            result.add(buf.toString());
+            buf = new StringBuffer();
+        }
+        return result;
+    }
+}
--- nc150/TopKElements.java
+++ nc150/TopKElements.java
@@ -1,2 +1,103 @@
-package PACKAGE_NAME;public class TopKElements {
+import java.util.*;
+
+/**
+ * Top K Elements in List
+ * Solved
+ * Given an integer array nums and an integer k, return the k most frequent elements within the array.
+ *
+ * The test cases are generated such that the answer is always unique.
+ *
+ * You may return the output in any order.
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,2,3,3,3], k = 2
+ *
+ * Output: [2,3]
+ * Example 2:
+ *
+ * Input: nums = [7,7], k = 1
+ *
+ * Output: [7]
+ * Constraints:
+ *
+ * 1 <= nums.length <= 10^4.
+ * -1000 <= nums[i] <= 1000
+ * 1 <= k <= number of distinct elements in nums.
+ */
+
+public class TopKElements {
+
+    /*
+     * Runtime Complexity for this one would be O(n log n)
+     */
+    public int[] topKFrequentV1(int[] nums, int k) {
+        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>((a,b) -> b.getValue() - a.getValue());
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+
+        // Put all element in the Map for tracking frequency
+        for(int n : nums) {
+            freqMap.put(n, freqMap.getOrDefault(n,0)+1);
+        }
+
+        // put all map entries in PQ to sort in the order of their freuqency
+        for (Map.Entry entry: freqMap.entrySet()) {
+            maxHeap.offer(entry);
+        }
+        int[] result = new int[k];
+
+        // retrieve top k elements till the Q runs out
+        for(int i=0; i< k; i++) {
+            if(!maxHeap.isEmpty()) {
+                result[i] = maxHeap.poll().getKey();
+            } else {
+                break;
+            }
+        }
+
+        return result;
+
+    }
+
+    // Runtime complexity of O(n)
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+
+        // creating a list array to store the frequency as index
+        // and corresponding nums in the list
+        // Reverse indexing the freqMap - thus can have multiple values for a frequency
+        List<Integer>[] freqArr = new List[nums.length+1];
+
+        for(int i = 0; i< freqArr.length; i++) {
+            freqArr[i] = new ArrayList<>();
+        }
+
+        // Put all element in the Map for tracking frequency
+        for(int n : nums) {
+            freqMap.put(n, freqMap.getOrDefault(n,0)+1);
+        }
+
+        // put all map entries in freqArr
+        for (Map.Entry<Integer, Integer> entry: freqMap.entrySet()) {
+            freqArr[entry.getValue()].add(entry.getKey());
+        }
+
+        int[] result = new int[k];
+
+        // iterate over the freqArr starting from back and fill result array
+        // while there is an entry in freqArr and all k places in result are filled
+        int rIndex = 0;
+        for(int i=freqArr.length-1; i>0 && rIndex < k; i--) {
+            // iterate over each freqArr entry ( if list not empty) to fill result
+            for(int n : freqArr[i]) {
+                result[rIndex] = n;
+                rIndex++;
+                if(rIndex==k) {
+                    return result;
+                }
+            }
+        }
+        return result;
+
+    }
 }
